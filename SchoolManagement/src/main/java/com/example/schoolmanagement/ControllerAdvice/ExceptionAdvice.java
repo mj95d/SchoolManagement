a891f05e28589b4836f6735b7ab08a25fe99211a
Abdulmajeed <132199458+mj95d@users.noreply.github.com>
2023-05-30 01:41:46 +0300

    ExceptionAdvice.java

--- SchoolManagement/src/main/java/com/example/schoolmanagement/ControllerAdvice/ExceptionAdvice.java
+++ SchoolManagement/src/main/java/com/example/schoolmanagement/ControllerAdvice/ExceptionAdvice.java
@@ -6,58 +6,56 @@ import org.springframework.dao.InvalidDataAccessResourceUsageException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.ConcurrentModificationException;
 
-@org.springframework.web.bind.annotation.ControllerAdvice
+@ControllerAdvice
 public class ExceptionAdvice {
 
-    @ExceptionHandler(ApiResponse.ApiResponse.class)
-    public ResponseEntity<ErrorResponse> handelExceptionApiResponse(ApiException e){
-        ErrorResponse error = new ErrorResponse(e.getMessage(), HttpStatus.BAD_REQUEST.value());
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<ApiResponse> handelExceptionApiResponse(ApiException e){
+        ApiResponse error = new ApiResponse(e.getMessage(), HttpStatus.BAD_REQUEST.value());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST.value()).body(error);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> MethodArgumentNotValidException(MethodArgumentNotValidException ex){
-        ErrorResponse error = new ErrorResponse(ex.getFieldError().getDefaultMessage(), HttpStatus.BAD_REQUEST.value());
+    public ResponseEntity<ApiException> MethodArgumentNotValidException(MethodArgumentNotValidException ex){
+        ApiException error = new ApiResponse(ex.getFieldError().getDefaultMessage(), HttpStatus.BAD_REQUEST.value());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
     @ExceptionHandler(value = ConcurrentModificationException.class)
-    public ResponseEntity<ErrorResponse> ConstraintViolationException(ConcurrentModificationException e) {
-        ErrorResponse error = new ErrorResponse(e.getMessage(), HttpStatus.BAD_REQUEST.value());
+    public ResponseEntity<ApiException> ConstraintViolationException(ConcurrentModificationException e) {
+        ApiException error = new ApiResponse(e.getMessage(), HttpStatus.BAD_REQUEST.value());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
     @ExceptionHandler(value = SQLIntegrityConstraintViolationException.class)
-    public ResponseEntity<ErrorResponse> SQLIntegrityConstraintViolationException(SQLIntegrityConstraintViolationException e){
-        ErrorResponse error = new ErrorResponse(e.getMessage(), HttpStatus.BAD_REQUEST.value());
+    public ResponseEntity<ApiException> SQLIntegrityConstraintViolationException(SQLIntegrityConstraintViolationException e){
+        ApiException error = new ApiException(e.getMessage(), HttpStatus.BAD_REQUEST.value());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
     @ExceptionHandler(value = HttpRequestMethodNotSupportedException.class)
-    public ResponseEntity<ErrorResponse> HttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
-        ErrorResponse error = new ErrorResponse(e.getMessage(), HttpStatus.BAD_REQUEST.value());
+    public ResponseEntity<ApiException> HttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+        ApiException error = new ApiResponse(e.getMessage(), HttpStatus.BAD_REQUEST.value());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
 
     @ExceptionHandler(value = InvalidDataAccessResourceUsageException.class )
-    public ResponseEntity<ErrorResponse> InvalidDataAccessResourceUsageException(InvalidDataAccessResourceUsageException e){
-        ErrorResponse error = new ErrorResponse(e.getMessage(), HttpStatus.BAD_REQUEST.value());
+    public ResponseEntity<ApiException> InvalidDataAccessResourceUsageException(InvalidDataAccessResourceUsageException e){
+        ApiException error = new ApiResponse(e.getMessage(), HttpStatus.BAD_REQUEST.value());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
     @ExceptionHandler(value = HttpMediaTypeNotAcceptableException.class)
-    public ResponseEntity<ErrorResponse> HttpMessageNotReadableException(HttpMessageNotReadableException e) {
-        ErrorResponse error = new ErrorResponse(e.getMessage(), HttpStatus.BAD_REQUEST.value());
+    public ResponseEntity<ApiException> HttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        ApiException error = new ApiResponse(e.getMessage(), HttpStatus.BAD_REQUEST.value());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
